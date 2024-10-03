af45d224071af308ed47cf6f6a8499f612a5cacc
Jaef09 <javieralejandroecheverria@gmail.com>
2024-10-02 21:40:22 -0600

    ruta

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Index } from './Index'
+/* import { Index } from './Index' */
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Index />
+  {/*   <Index /> */}
   </React.StrictMode>,
 )
