71c5215aa85176973c97ab28650c0977fd87ad89
Jaef09 <javieralejandroecheverria@gmail.com>
2024-10-02 21:35:21 -0600

    mejra

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css'
-import { HomePage } from './pages/HomePage/HomePage'
-import { LoginPage } from './pages/LoginPage/LoginPage'
+import { useState } from 'react';
+import { Outlet, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
+import { HomePage } from './pages/HomePage/HomePage';
+import { LoginPage } from './pages/LoginPage/LoginPage';
 
-export const App = ()=> {
-  
-
+export const App = () => {
   return (
-    
     <>
-    <Router basename="/landingPage-ClinLink">
-      <Routes>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={LoginPage} />
-        {/* Añade más rutas según sea necesario */}
-      </Routes>
-    </Router>
+      <Router basename="/landingPage-ClinLink">
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/about" element={<LoginPage />} />
+          {/* Añade más rutas según sea necesario */}
+        </Routes>
+      </Router>
       <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
