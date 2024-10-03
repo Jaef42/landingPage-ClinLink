8137f0c7888eec9c70225349650960f82764e89b
Jaef09 <javieralejandroecheverria@gmail.com>
2024-10-03 09:18:54 -0600

    Actualizaciones

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { SideBar } from '../../components/SideBar/SideBar';
-import './HomePage.css';
 import HORAS from '../../assets/HORAS.png';
 import LOGO from '../../assets/LOGO.png';
 import SEGURIDAD from '../../assets/SEGURIDAD.png';
@@ -11,9 +10,8 @@ export const HomePage = () => {
     const location = useLocation();
 
     useEffect(() => {
-        // Verifica si la URL contiene el hash y desplaza hacia el elemento correspondiente
         if (location.hash) {
-            const element = document.getElementById(location.hash.substring(1)); // Eliminar '#' del hash
+            const element = document.getElementById(location.hash.substring(1));
             if (element) {
                 element.scrollIntoView({ behavior: 'smooth' });
             }
@@ -22,131 +20,120 @@ export const HomePage = () => {
 
     return (
         <>
-            <div className="contenido-general">
+            <div className="flex">
                 <div>
                     <SideBar />
                 </div>
 
-                <div className="contenido">
-                    <div className="home-page">
-                        <main className="main-content">
-                            <section className="hero-section">
-                                <div className="hero-image-container">
+                <div className="flex-1" style={{ marginLeft: '5.3rem', overflow: 'auto' }}>
+                    <div className="text-center bg-white w-[102.5%] -ml-5 -mt-2">
+                        <main>
+                            <section className="relative mb-10">
+                                <div className="relative flex w-full h-[75vh] bg-cover bg-center bg-no-repeat bg-gradient-to-t from-black/70 to-black/70" 
+                                    style={{ 
+                                    backgroundImage: "url(https://www.provincial.com/content/dam/public-web/venezuela/photos/marquees/card-prestamo-personal.im1604338031977im.jpg?imwidth=1600)", 
+                                    filter: "brightness(0.2)" 
+                                    }}>
                                 </div>
-                                <div className="hero-content">
-                                    <h2><strong></strong>Bienvenido a CLINCLINK</h2>
+                                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+                                    <h2 className="text-4xl font-bold">Bienvenido a CLINCLINK</h2>
                                     <p>Ofrecemos servicios clínicos seguros y confiables para satisfacer tus necesidades de salud.</p>
                                 </div>
                             </section>
 
-                            {/* Encabezado "Servicios" agregado aquí */}
-                            <h2 className="section-heading1" id='servicios'><strong>SERVICIOS</strong></h2>
-                            <section className="features-section">
-                                <div className="feature">
-                                    <img src={'https://magnetosur.com/wp-content/uploads/2021/11/Que-diferencias-existen-entre-la-medicina-general-y-la-medicina-interna.jpg'} alt="Servicios Financieros" />
-                                    <h3>CONSULTAS MÉDICAS GENERALES</h3>
+                            {/* Servicios */}
+                            <h2 className="text-5xl mb-12 text-[#5b7ce9] font-bold" id="servicios">SERVICIOS</h2>
+                            <section className="flex justify-center mb-10">
+                                <div className="flex-1 text-center p-5">
+                                    <img className="mx-auto mb-5 rounded-xl max-w-xs" src={'https://magnetosur.com/wp-content/uploads/2021/11/Que-diferencias-existen-entre-la-medicina-general-y-la-medicina-interna.jpg'} alt="Servicios Financieros" />
+                                    <h3 className="text-xl font-semibold">CONSULTAS MÉDICAS GENERALES</h3>
                                     <p>Evaluación y tratamiento de afecciones comunes por médicos de atención primaria.</p>
                                 </div>
-                                <div className="feature">
-                                    <img src={'https://www.polizamedica.es/images/noticias/ramas-de-la-medicina-preventiva.jpg'} alt="Seguridad" />
-                                    <h3>MEDICINA PREVENTIVA</h3>
+                                <div className="flex-1 text-center p-5">
+                                    <img className="mx-auto mb-5 rounded-xl max-w-xs" src={'https://www.polizamedica.es/images/noticias/ramas-de-la-medicina-preventiva.jpg'} alt="Seguridad" />
+                                    <h3 className="text-xl font-semibold">MEDICINA PREVENTIVA</h3>
                                     <p>Servicios enfocados en la prevención de enfermedades a través de chequeos regulares y educación en salud.</p>
                                 </div>
-                                <div className="feature">
-                                    <img src={'https://medlineplus.gov/images/LaboratoryTests_share.jpg'} alt="Atención al Cliente" />
-                                    <h3>EXÁMENES DE LABORATORIO</h3>
+                                <div className="flex-1 text-center p-5">
+                                    <img className="mx-auto mb-5 rounded-xl max-w-xs" src={'https://medlineplus.gov/images/LaboratoryTests_share.jpg'} alt="Atención al Cliente" />
+                                    <h3 className="text-xl font-semibold">EXÁMENES DE LABORATORIO</h3>
                                     <p>Pruebas diagnósticas para evaluar la salud, incluyendo análisis de sangre y muestras biológicas.</p>
                                 </div>
                             </section>
 
-                            <section className="features-section">
-                                <div className="feature">
-                                    <img src={'https://principal.url.edu.gt/wp-content/uploads/2023/08/radio1_thumbns.jpg'} alt="Servicios Financieros" />
-                                    <h3>RADIOLOGÍA Y ESTUDIOS DE IMAGEN</h3>
+                            {/* Otra sección de Servicios */}
+                            <section className="flex justify-center mb-10">
+                                <div className="flex-1 text-center p-5">
+                                    <img className="mx-auto mb-5 rounded-xl max-w-xs" src={'https://principal.url.edu.gt/wp-content/uploads/2023/08/radio1_thumbns.jpg'} alt="Servicios Financieros" />
+                                    <h3 className="text-xl font-semibold">RADIOLOGÍA Y ESTUDIOS DE IMAGEN</h3>
                                     <p>Uso de tecnologías como rayos X, tomografías y ultrasonidos para diagnóstico de condiciones internas.</p>
                                 </div>
-                                <div className="feature">
-                                    <img src={'https://www.paho.org/sites/default/files/styles/max_1300x1300/public/card/2021-04/vaccination-efficacy-650-400.jpg?itok=DBUAd_L5'} alt="Seguridad" />
-                                    <h3>VACUNACIÓN</h3>
+                                <div className="flex-1 text-center p-5">
+                                    <img className="mx-auto mb-5 rounded-xl max-w-xs" src={'https://www.paho.org/sites/default/files/styles/max_1300x1300/public/card/2021-04/vaccination-efficacy-650-400.jpg?itok=DBUAd_L5'} alt="Seguridad" />
+                                    <h3 className="text-xl font-semibold">VACUNACIÓN</h3>
                                     <p>Aplicación de vacunas para prevenir enfermedades infecciosas, siguiendo los calendarios de inmunización.</p>
                                 </div>
-                                <div className="feature">
-                                    <img src={'https://www.altamed.org/sites/default/files/2023-09/hospital.jpg'} alt="Atención al Cliente" />
-                                    <h3>ATENCIÓN DE URGENCIAS</h3>
+                                <div className="flex-1 text-center p-5">
+                                    <img className="mx-auto mb-5 rounded-xl max-w-xs" src={'https://www.altamed.org/sites/default/files/2023-09/hospital.jpg'} alt="Atención al Cliente" />
+                                    <h3 className="text-xl font-semibold">ATENCIÓN DE URGENCIAS</h3>
                                     <p>Servicios médicos inmediatos para tratar lesiones y enfermedades agudas que requieren atención rápida.</p>
                                 </div>
                             </section>
 
-                            <div className="content-section-a">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-lg-5 col-lg-offset-1 col-sm-push-6 col-sm-6">
-                                            <div className="clearfix"></div>
-                                            <h2 className="section-heading" id='nosotros'><strong>SOBRE NOSOTROS</strong></h2>
-                                            <hr className="section-heading-spacer"></hr>
-                                            <div className="clearfix"></div>
-                                            <p className="lead">
-                                                En nuestra clínica, nos dedicamos a proporcionar atención médica de alta calidad, enfocándonos en la salud integral de nuestros pacientes. Contamos con un equipo de profesionales altamente capacitados en diversas especialidades médicas, comprometidos con ofrecer un trato humano y personalizado.
-                                            </p>
-                                        </div>
-                                        <div className="col-lg-5 col-sm-pull-6 col-sm-6">
-                                            <img className="img-responsive" src={LOGO} alt="" />
-                                        </div>
+                            {/* Sobre Nosotros */}
+                            <div className="py-20 bg-[#FCFBF9]">
+                                <div className="max-w-5xl mx-auto flex">
+                                    <div className="w-1/2 text-left pr-10">
+                                        <h2 className="text-4xl font-bold mb-5 text-[#5b7ce9]" id='nosotros'>SOBRE NOSOTROS</h2>
+                                        <p className="text-xl">En nuestra clínica, nos dedicamos a proporcionar atención médica de alta calidad, enfocándonos en la salud integral de nuestros pacientes. Contamos con un equipo de profesionales altamente capacitados en diversas especialidades médicas.</p>
+                                    </div>
+                                    <div className="w-1/2">
+                                        <img className="max-w-sm mx-auto" src={LOGO} alt="Logo" />
                                     </div>
                                 </div>
                             </div>
 
-                            <div className="content-section-a">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-lg-5 col-lg-offset-1 col-sm-push-6 col-sm-6">
-                                            <div className="clearfix"></div>
-                                            <h2 className="section-heading"><strong>SEGURIDAD</strong></h2>
-                                            <hr className="section-heading-spacer"></hr>
-                                            <div className="clearfix"></div>
-                                            <p className="lead">En nuestra clínica, la seguridad y el bienestar de nuestros pacientes son nuestra máxima prioridad. Contamos con rigurosos protocolos de higiene y control de infecciones, asegurando un entorno limpio y seguro para todos.</p>
-                                        </div>
-                                        <div className="col-lg-5 col-sm-pull-6 col-sm-6">
-                                            <img className="img-responsive" src={SEGURIDAD} alt="" />
-                                            <img className="img-responsive" src="https://www.simple.com/assets/images/simple-kit.png" id="responsive" alt="" />
-                                        </div>
+                            {/* Seguridad */}
+                            <div className="py-20 bg-[#FCFBF9]">
+                                <div className="max-w-5xl mx-auto flex">
+                                    <div className="w-1/2 text-left pr-10">
+                                        <h2 className="text-4xl font-bold mb-5 text-[#5b7ce9]" id='seguridad'>SEGURIDAD</h2>
+                                        <p className="text-xl">En nuestra clínica, la seguridad y el bienestar de nuestros pacientes son nuestra máxima prioridad. Contamos con rigurosos protocolos de higiene y control de infecciones.</p>
+                                    </div>
+                                    <div className="w-1/2">
+                                        <img className="max-w-sm mx-auto" src={SEGURIDAD} alt="Seguridad" />
                                     </div>
                                 </div>
                             </div>
 
-                            <div className="content-section-d">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-lg-5 col-sm-6">
-                                            <h2 className="section-heading-white"><strong>AGENDA TUS CITAS EN LÍNEA</strong></h2>
-                                            <hr className="section-heading-spacer-white" />
-                                            <div className="clearfix"></div>
-                                            <p className="lead-white">En nuestra clínica, facilitamos tu acceso a la atención médica mediante nuestro sistema de citas en línea. Ahora puedes programar tu consulta de manera rápida y sencilla desde la comodidad de tu hogar, a cualquier hora del día.</p>
-                                        </div>
-                                        <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                                            <img className="img-responsive2" src={CITAS} alt="" />
-                                        </div>
+                            {/* Citas Online */}
+                            <div className="py-20 bg-[#5b7ce9] text-white">
+                                <div className="max-w-5xl mx-auto flex">
+                                    <div className="w-1/2">
+                                        <h2 className="text-4xl font-bold mb-5">AGENDA TUS CITAS EN LÍNEA</h2>
+                                        <p className="text-xl">En nuestra clínica, facilitamos tu acceso a la atención médica mediante nuestro sistema de citas en línea.</p>
+                                    </div>
+                                    <div className="w-1/2">
+                                        <img className="max-w-sm mx-auto" src={CITAS} alt="Citas" />
                                     </div>
                                 </div>
                             </div>
-                            <div className="content-section-b">
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-lg-5 col-sm-6">
-                                            <h2 className="section-heading"><strong>DISPONIBILIDAD 24/7</strong></h2>
-                                            <hr className="section-heading-spacer"></hr>
-                                            <div className="clearfix"></div>
-                                            <p>Puedes acceder a nuestro sistema de citas en línea las 24 horas del día, los 7 días de la semana, para agendar tu consulta en el momento que más te convenga.</p>
-                                        </div>
-                                        <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                                            <img className="img-responsive3" src={HORAS} alt="" />
-                                        </div>
+
+                            {/* Disponibilidad */}
+                            <div className="py-20 bg-white">
+                                <div className="max-w-5xl mx-auto flex">
+                                    <div className="w-1/2">
+                                        <h2 className="text-4xl font-bold mb-5 text-[#5b7ce9]">DISPONIBILIDAD 24/7</h2>
+                                        <p className="text-xl">Puedes acceder a nuestro sistema de citas en línea las 24 horas del día, los 7 días de la semana.</p>
+                                    </div>
+                                    <div className="w-1/2">
+                                        <img className="max-w-sm mx-auto" src={HORAS} alt="Horas" />
                                     </div>
                                 </div>
                             </div>
 
                         </main>
-                        <footer className="footer">
+                        <footer className="py-5 bg-[#5b7ce9] text-center text-white">
                             <p>&copy; {new Date().getFullYear()} ClincLink. Todos los derechos reservados.</p>
                         </footer>
                     </div>
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
-import { SideBar } from '../../components/SideBar/SideBar'
-import './login.css'
+import React from 'react';
+import { SideBar } from '../../components/SideBar/SideBar';
+import logo from '../../assets/logotransp.png';
 
 export const LoginPage = () => {
   return (
-    <>
-      <div className='contenedor-general'>
-        <div className=''>
-          {/* Aquí está tu barra de navegación */}
-          <SideBar />
-        </div>
-        <div className='contenido'>
-          <div className="login-lg"></div>
-          <div className="login-block">
-            <h1>Login</h1>
-            <input name='username' type="text" placeholder="Username" id="username" />
-            <input name='password' type="password" placeholder="Password" id="password" />
-            <button>Iniciar</button>
+    <div className="flex h-screen items-center justify-center bg-gray-200">
+      <div className="w-full max-w-xs">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="text-center text-2xl font-bold mb-6">ClinLink</h1>
+          <div className="flex justify-center mb-6">
+            <img 
+             src={logo} 
+             alt="Logo de ClinLink" 
+              className="w-16 h-16 object-contain rounded-full"
+            />  
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Usuario
+            </label>
+            <input
+              name="username"
+              type="text"
+              placeholder="Usuario"
+              id="username"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              id="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Login
+            </button>
+          </div>
+          <div className="text-center mt-6">
+            <p className="text-sm text-gray-600">¿Aún no estás registrado?</p>
+            <button className="mt-2 bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Regístrate
+            </button>
           </div>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
