81fd8289efeef63ab19d7ce3ed15606e73e7184c
Jaef09 <javieralejandroecheverria@gmail.com>
2024-10-02 21:27:07 -0600

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import { Outlet } from 'react-router-dom'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css'
 
 export const App = ()=> {
   
 
   return (
+    
     <>
+    <Router basename="/landingPage-ClinLink">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        {/* Añade más rutas según sea necesario */}
+      </Switch>
+    </Router>
       <Outlet />
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Index } from './Index'
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <App />
     <Index />
   </React.StrictMode>,
 )
