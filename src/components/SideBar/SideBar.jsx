bfadcf51054ba65e038bd757bbd2fbfe763b94bd
Jaef09 <javieralejandroecheverria@gmail.com>
2024-10-03 11:52:11 -0600

    redireccion

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -8,6 +8,9 @@ import './sideBar.css'
 export const SideBar = () => {
 
     const { loggedIn, setLoggedIn, dataUser, setDataUser } = useContext(NombreContexto);
+    const handleRegisterClick = () => {
+        window.open("https://clin-link-system.vercel.app/register", "_blank");
+    };
 
     return (
         <>
@@ -54,12 +57,12 @@ export const SideBar = () => {
                         </li>
 
                         <li className='li'>
-                            <Link to={{ pathname: "https://clin-link-system.vercel.app/register" }} target="_blank" className='link'>
+                        <button onClick={handleRegisterClick} className='link' style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24">
                                     <path fill="#5b7ce9" d="M15 4a4 4 0 0 0-4 4a4 4 0 0 0 4 4a4 4 0 0 0 4-4a4 4 0 0 0-4-4m0 1.9a2.1 2.1 0 1 1 0 4.2A2.1 2.1 0 0 1 12.9 8A2.1 2.1 0 0 1 15 5.9M4 7v3H1v2h3v3h2v-3h3v-2H6V7zm11 6c-2.67 0-8 1.33-8 4v3h16v-3c0-2.67-5.33-4-8-4m0 1.9c2.97 0 6.1 1.46 6.1 2.1v1.1H8.9V17c0-.64 3.1-2.1 6.1-2.1" />
                                 </svg>
                                 <span className='nav-item'>Regístrate</span>
-                            </Link>
+                            </button>
                         </li>
 
                         <li className='li'>
